c245c9916f322109999e4d78bcbcb0fa84b1e675
Aleksandr Klyuchko <avkluchko@yandex.ru>
2021-02-05 23:01:29 +0500

    Added version

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+#include "tutorial-config.h"
+
 using namespace std;
 
-int main() {
-    cout << "Hello!" << endl;
-
-    return 0;
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        // report version
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+                  << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
 }
