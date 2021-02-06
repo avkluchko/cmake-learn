6bcb738ab6e6c8cd748c5e859c4fc9916b4fd219
Aleksandr Klyuchko <avkluchko@yandex.ru>
2021-02-06 19:46:04 +0500

    Added code

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
+#ifdef USE_MYMATH
+#   include "MathFunctions.h"
+#endif
+
 #include "tutorial-config.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
+#ifdef USE_MYMATH
+    const double outputValue = mysqrt(inputValue);
+#else
+    const double outputValue = sqrt(inputValue);
+#endif
+
     if (argc < 2) {
         // report version
         std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
